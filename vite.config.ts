14ad3188a5b2cd2d6c7eb8f7f7f71896faf5a981
keira <artist2819@gmail.com>
2025-08-28 19:21:47 -0400

    chore: update react and related dependencies to v19.1.1
    - Upgrade react and react-dom to v19.1.1
    - Update scheduler to v0.26.0
    - Replace minified production builds with unminified versions
    - Update copyright notices from Facebook to Meta Platforms
    - Update documentation links from reactjs.org to react.dev
    - Simplify vite config by removing unnecessary build options

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react({
-      jsxRuntime: 'automatic'
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    target: 'esnext',
-    minify: 'esbuild',
-    sourcemap: false,
+  server: {
+    host: "::",
+    port: 8080,
   },
 });
